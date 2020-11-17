f0795ada12be0cd5b7c7ed314b38bd5f01d97b7c
Henrique <henriquemarrocos@hotmail.com>
2020-11-16 23:06:19 -0500

    Updates

---
+++ precache-manifest.02c54ed4f90872633f9c52cc71004691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8904b668e73cd6313b6139d598a16ed",
+    "url": "/henriflix/index.html"
+  },
+  {
+    "revision": "56250ff3f784769d039a",
+    "url": "/henriflix/static/css/main.fdd14aa9.chunk.css"
+  },
+  {
+    "revision": "618eb3fcabbc255ad8a0",
+    "url": "/henriflix/static/js/2.403b306f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/henriflix/static/js/2.403b306f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56250ff3f784769d039a",
+    "url": "/henriflix/static/js/main.f30c4f8f.chunk.js"
+  },
+  {
+    "revision": "782531f8e243696e0a9c",
+    "url": "/henriflix/static/js/runtime-main.bf22fca0.js"
+  }
+]);
